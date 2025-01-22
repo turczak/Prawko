1bfb5bc513ef8d027f4742711879fc40ee9756b4
turczak <wojciech.turczak97@gmail.com>
2025-01-22 18:08:41 +0100

    little fix for showing roles in JSON

--- src/main/java/pl/turlap/prawko/controllers/RoleController.java
+++ src/main/java/pl/turlap/prawko/controllers/RoleController.java
@@ -3,14 +3,15 @@ package pl.turlap.prawko.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import pl.turlap.prawko.dto.RoleDto;
 import pl.turlap.prawko.services.RoleService;
 
 import java.util.List;
 
-@RestController
-@RequestMapping(path = "/roles")
+@Controller
+@RequestMapping("/roles")
 public class RoleController {
 
     private final RoleService roleService;
@@ -21,6 +22,7 @@ public class RoleController {
     }
 
     @GetMapping(path = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
     public List<RoleDto> findALlRoles() {
         return roleService.findAll();
     }
--- src/main/java/pl/turlap/prawko/models/Role.java
+++ src/main/java/pl/turlap/prawko/models/Role.java
@@ -2,14 +2,18 @@ package pl.turlap.prawko.models;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity(name = "role")
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
